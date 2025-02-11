1202e88729c360944cc962a2540ba488304a3d1a
sonatejasd <sonatejasd@gmail.com>
2025-02-11 21:38:57 +0530

    first commit

---
+++ src/app/register/page.tsx
@@ -0,0 +1,2 @@
+import { register } from '@/components';
+export default register;
---
+++ src/components/index.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useRef } from 'react'
+
+export const register = () => {
+  const name = useRef(null);
+  const rno = useRef(null);
+
+  const registerStudent = async () => {
+    try{
+    const res = await fetch("http://localhost:2020/students/register",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        data: {
+          name: name.current?.value,
+          rno: rno.current?.value
+        }
+      })
+    } )
+  }catch(err) {
+    console.log(err);
+  }
+  };
+  return (
+    <div className='d-flex justify-content-center align-items-center h-100'>
+      <div>
+      <div>
+        <label className="form-label" htmlFor="name">Name:</label>
+        <input ref={name} type="text" className='form-control' id='name'/>
+      </div>
+      <div className='mt-2 mb-2'>
+        <label htmlFor="rno" className='form-label'>Roll No.</label>
+        <input ref={rno} type="text" className='form-control'/>
+      </div>
+      <div><button className='btn btn-primary' onClick={registerStudent}>register</button></div>
+      </div>
+    </div>
+  )
+}
+
+
